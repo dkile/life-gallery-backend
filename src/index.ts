e2a4d9b9bb4b020470ee3777489d8a88c9e7924d
dkile <dkile532@naver.com>
2022-01-24 22:32:58 +0900

    feat: add "register article" button

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify
   logger: { level: "info", file: "./server.log" }
 });
 
+const extractImageUrl = (rawUrl: string) => rawUrl.split("(")[1].split(")")[0];
+
 // test code
 server.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
   return { hello: "world" };
@@ -16,6 +18,7 @@ server.post("/image", async (request: FastifyRequest, reply: FastifyReply) => {
   server.log.info("------------------------------------------------------------------");
   server.log.info(request.body);
   server.log.info("------------------------------------------------------------------");
+  const anyBody = request.body as any;
   return {
     version: "2.0",
     template: {
@@ -23,6 +26,18 @@ server.post("/image", async (request: FastifyRequest, reply: FastifyReply) => {
         {
           simpleText: {
             text: "이미지 등록이 완료되었습니다."
+          },
+          basicCard: {
+            title: "글귀 등록",
+            thumbnail: extractImageUrl(anyBody.detailParams.secureimage.origin),
+            buttons: [
+              {
+                label: "글귀 등록",
+                action: "block",
+                messageText: "글귀를 등록해주세요",
+                blockId: "61ee439fb8ad900c148e5e76"
+              }
+            ]
           }
         }
       ]
