e6a31ffc8c43e2a3a67d5fc37298d30c47f9971e
dkile <dkile532@naver.com>
2022-01-26 01:57:38 +0900

    feat: connect db with router

--- src/routers/image.ts
+++ src/routers/image.ts
@@ -1,11 +1,25 @@
-import { ServerType } from "../types/type";
+import { ServerType, imageRequestBody } from "../types/type";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { findUserByKakaoId, saveUser } from "../service/user";
+import { User } from "../entity/user";
 
 const imageRouter = async (server: ServerType) => {
-  server.post("/image", async (req: FastifyRequest, res: FastifyReply) => {
+  server.post("/image", async (req: FastifyRequest<any>, res: FastifyReply) => {
+    const requestBody: imageRequestBody = req.body;
+
+    server.log.info("----------------------IMAGE------------------------");
+    server.log.info(requestBody);
     server.log.info("---------------------------------------------------");
-    server.log.info(req.body);
-    server.log.info("---------------------------------------------------");
+
+    const user = await findUserByKakaoId(server, requestBody.userRequest.user.id);
+    if (!user) {
+      const newUser = new User();
+      newUser.kakao_id = requestBody.userRequest.user.id;
+      newUser.full_name = " ";
+      newUser.nick_name = " ";
+      newUser.user_state = 1;
+      await saveUser(server, newUser);
+    }
     res.send({
       version: "2.0",
       template: {
--- src/routers/register.ts
+++ src/routers/register.ts
@@ -1,36 +1,64 @@
-import { ServerType } from "../types/type";
+import { ServerType, registerRequestBody } from "../types/type";
 import { FastifyRequest, FastifyReply } from "fastify";
+import { findUserByKakaoId } from "../service/user";
 
 const registerRouter = async (server: ServerType) => {
-  server.post("/register", async (req: FastifyRequest, res: FastifyReply) => {
+  server.post("/register", async (req: FastifyRequest<any>, res: FastifyReply) => {
+    const requestBody: registerRequestBody = req.body;
 
+    server.log.info("----------------------REGISTER----------------------");
+    server.log.info(requestBody);
     server.log.info("---------------------------------------------------");
-    server.log.info(req.body);
-    server.log.info("---------------------------------------------------");
-    res.send({
-      version: "2.0",
-      template: {
-        outputs: [
-          {
-            simpleText:
-              "작품 등록 절차를 진행합니다. 개인 정보 제공 동의 후 이미지 업로드 단계로 넘어갑니다. 개인 정보 제공에 동의하시겠습니까?"
-          }
-        ],
-        quickReplies: [
-          {
-            label: "개인 정보 제공 동의",
-            action: "block",
-            messageText: "개인 정보 제공에 동의할게요",
-            blockId: "61ee39c2bce5c65875ef3a2e"
-          },
-          {
-            label: "개인 정보 제공 비동의",
-            action: "message",
-            messageText: "개인 정보 제공에 동의하지 않아요."
-          }
-        ]
-      }
-    });
+
+    const user = await findUserByKakaoId(server, requestBody.userRequest.user.id);
+    if (!user) {
+      res.send({
+        version: "2.0",
+        template: {
+          outputs: [
+            {
+              simpleText:
+                "작품 등록 절차를 진행합니다. 개인 정보 제공 동의 후 이미지 업로드 단계로 넘어갑니다. 개인 정보 제공에 동의하시겠습니까?"
+            }
+          ],
+          quickReplies: [
+            {
+              label: "개인 정보 제공 동의",
+              action: "block",
+              messageText: "개인 정보 제공에 동의할게요",
+              blockId: "61ee39c2bce5c65875ef3a2e" // image upload block
+            },
+            {
+              label: "개인 정보 제공 비동의",
+              action: "message",
+              messageText: "개인 정보 제공에 동의하지 않아요."
+            }
+          ]
+        }
+      });
+    } else {
+      res.send({
+        version: "2.0",
+        template: {
+          outputs: [
+            {
+              basicCard: {
+                thumbnail: {
+                  imageUrl: "https://og-image.vercel.app/Welcome%20to%20life%20gallery.png"
+                },
+                buttons: [
+                  {
+                    action: "block",
+                    label: "이미지 업로드",
+                    blockId: "61ee39c2bce5c65875ef3a2e"
+                  }
+                ]
+              }
+            }
+          ]
+        }
+      });
+    }
   });
 };
 
