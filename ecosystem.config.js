fc0a1280429c2f320a28f9d369aa256775fa7b0a
d0lim <limdongyoung0@gmail.com>
2022-01-26 23:09:23 +0900

    fix: production environment setting on pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,9 @@ module.exports = {
   instances: 2,
   exec_mode: "cluster",
   autorestart: true,
+  env: {
+    NODE_ENV: "production" // 개발환경시 적용될 설정 지정
+  },
 
   output: "~/logs/pm2/console.log",
   error: "~/logs/pm2/consoleError.log"
